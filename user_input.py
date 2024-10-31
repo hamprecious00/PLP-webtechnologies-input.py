33c93786128ba840b70a7c386c60c92866780cd7
Hamphrey Wafula <hamprecious30@gmail.com>
2024-10-31 19:29:02 +0300

    added file

---
+++ user_input.py
@@ -0,0 +1,10 @@
+# user_input.py
+
+# User input
+name = input("What is your name? ")
+age = input("How old are you? ")
+location = input("Where do you live? ")
+
+# Print message
+
+print(f"Hello {name}, you are {age} years old and live in {location}.")
